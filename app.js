1cddbdd05be576fdfe91229fdd93194f4c77bebf
MatviyO <33325458+MatviyO@users.noreply.github.com>
2019-12-17 22:22:38 +0200

    add213

--- app.js
+++ app.js
@@ -15,12 +15,25 @@ new Vue({
         cars: cars,
         car: cars[0],
         selectedCarIndex: 0,
-        phoneVisibility: false
+        phoneVisibility: false,
+        search: '',
+        modalVisibility: false
     },
     methods: {
         selectCar: function(index) {
             this.car = cars[index];
             this.selectedCarIndex = index;
+        }
+    },
+    computed: {
+        phoneBtnText() {
+            return this.phoneVisibility ? 'Hide phone' : 'Show phone'
+        },
+        filtersCars() {
+            const filtered = this.cars.filter((car) => {
+                return car.name.indexOf(this.search) > -1
+            })
+           return filtered
         }
     }
 
