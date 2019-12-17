e43a3c91c26107ff02962242e76714d45ac6b598
MatviyO <33325458+MatviyO@users.noreply.github.com>
2019-12-17 12:47:10 +0200

    add2x

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const car = (name, model, owner,year, phone, img) =>
+    ({name, model, owner,year, phone, img})
+
+const cars = [
+    car('Bmw', '4 series', 'Oleh', '2015', '+3809311111', 'images/1.jpg'),
+    car('Mercedes-benz', 'CLA', 'MyTwo', '2014', '+3809311122', 'images/2.jpeg'),
+    car('Bmw', '3 series', 'MyThree', '2015', '+3809311333', 'images/3.jpg')
+
+]
+
+new Vue({
+    el: '#app',
+    data: {
+        cars: cars,
+        car: cars[0]
+    },
+    methods: {
+        selectCar: function(index) {
+
+            this.car = cars[index];
+        }
+    }
+})
