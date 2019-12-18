b7f21b061957991b7d1ba4ebe74450f0781f5557
MatviyO <33325458+MatviyO@users.noreply.github.com>
2019-12-18 12:15:51 +0200

    addtransf

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-
+const log = (text,type, date = new Date()) => ({
+    text,type,date
+})
 const car = (name, model, owner,year, phone, img) =>
     ({name, model, owner,year, phone, img})
 
@@ -17,12 +19,25 @@ new Vue({
         selectedCarIndex: 0,
         phoneVisibility: false,
         search: '',
-        modalVisibility: false
+        modalVisibility: false,
+        logs: []
     },
     methods: {
-        selectCar: function(index) {
+        selectCar (index) {
             this.car = cars[index];
             this.selectedCarIndex = index;
+        },
+        newOrder() {
+            this.modalVisibility = false;
+            this.logs.push(
+                log(`Success order: ${this.car.name} - ${this.car.model}`, 'ok')
+            )
+        },
+        cancelOrder() {
+            this.modalVisibility = false;
+            this.logs.push(
+                log(`Canceled order: ${this.car.name} - ${this.car.model}`, 'cnl')
+            )
         }
     },
     computed: {
@@ -34,6 +49,12 @@ new Vue({
                 return car.name.indexOf(this.search) > -1
             })
            return filtered
+        }
+    },
+    filters: {
+        datte1(value) {
+
+            return value.toTimeString()
         }
     }
 
